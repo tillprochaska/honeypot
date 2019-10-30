27f58b51f01e88f650c52aa794086bea92e5d55f
Till Prochaska <tillprochaska@gmail.com>
2019-10-30 18:18:03 +0100

    Do not create new diary entries anymore

--- config/schedule.rb
+++ config/schedule.rb
@@ -13,36 +13,36 @@ every 1.minutes do
   rake 'cache:delete'
 end
 
-%w[00:10am 6:10am 9:10am 1:10pm 6:10pm].each do |time|
-  every :day, at: time do
-    rake 'archive'
-  end
-end
+# %w[00:10am 6:10am 9:10am 1:10pm 6:10pm].each do |time|
+#   every :day, at: time do
+#     rake 'archive'
+#   end
+# end
 
-every :hour, at: 5 do
-  rake 'wolf_waagen_api:fetch'
-end
+# every :hour, at: 5 do
+#   rake 'wolf_waagen_api:fetch'
+# end
 
-%w[09:10 14:10 19:10].each do |time|
-  every :day, at: time do
+# %w[09:10 14:10 19:10].each do |time|
+#   every :day, at: time do
     # ["Königreich Köln (Gimnich)", 4]
-    rake 'twitter:tweet[4]'
-  end
+#     rake 'twitter:tweet[4]'
+#   end
 end
 
-%w[09:40 14:40 19:40].each do |time|
-  every :day, at: time do
+# %w[09:40 14:40 19:40].each do |time|
+#   every :day, at: time do
     # ["Königreich Lage (Strulik)", 2]
-    rake 'twitter:tweet[2]'
-  end
-end
+#     rake 'twitter:tweet[2]'
+#  end
+# end
 
-%w[10:10 15:10 20:10].each do |time|
-  every :day, at: time do
+# %w[10:10 15:10 20:10].each do |time|
+#   every :day, at: time do
     # ["Königreich Witten (Marcel)", 3]
-    rake 'twitter:tweet[3]'
-  end
-end
+#     rake 'twitter:tweet[3]'
+#   end
+# end
 #
 # every 4.days do
 #   runner "AnotherModel.prune_old_records"
